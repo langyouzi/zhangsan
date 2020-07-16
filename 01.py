f7f2a70b5dc4dcc8ff7d83d9ec4e355d159492f6
langyouzi <975174636@qq.com>
2020-07-16 13:34:48 +0800

    第一次

---
+++ 01.py
@@ -0,0 +1,33 @@
+"""
+print("hello world!") #字符串
+print(2333)     #整数      
+print(2.3333)   #小数
+print(True)     #布尔值
+print(())       #元组
+print([])       #数组
+print({})       #字典
+
+床前明月光
+疑是地上霜
+举头望明月
+低头思故乡
+
+print("langzi",2333,0.2333)
+print("lang"+"zi")
+print("lang"*10)
+print(5*6%7/2)
+"""
+
+
+
+a = input("请输入:")
+b = input("请输入:")
+print("你输入的内容长度和是：",len(a+b))
+
+# print(type({}))
+
+# a = int("张三")
+# # print(a)
+
+# c ="哈哈哈哈"
+# print(len(c))
